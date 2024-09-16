c457e4a04941e30da900063f64f0cea2c4d01e95
Atlante <108891261+Atlante1952@users.noreply.github.com>
2024-09-16 18:38:09 +0200

    Add files via upload

--- script/messages_list.lua
+++ script/messages_list.lua
@@ -1,17 +1,19 @@
+local S = minetest.get_translator("atl_server_messages")
+
 atl_server_messages.messages_list = {
-    "-!- -!- Welcome to the server, please respect the rules established there.",
-    "-!- -!- Any form of nuisance, spam, insult and other is prohibited.",
-    "-!- Please respect the players on the server and their administrator.",
-    "-!- Enjoy your time on the server and have fun!",
-    "-!- If you have any questions, feel free to ask the moderators.",
-    "-!- Remember to report any issues or bugs you encounter.",
-    "-!- Be kind and helpful to new players.",
-    "-!- Cheating and hacking are strictly forbidden and will result in a ban.",
-    "-!- Respect the privacy and personal information of other players.",
-    "-!- Use appropriate language and avoid offensive content.",
-    "-!- Do not share personal or sensitive information on the server.",
-    "-!- Follow the instructions given by the server staff.",
-    "-!- Participate in server events and activities to enhance your experience.",
-    "-!- Help maintain a positive and friendly atmosphere on the server.",
-    "-!- Thank you for being a part of our community!"
+    S("-!- Welcome to the server, please respect the rules established there."),
+    S("-!- Any form of nuisance, spam, insult and other is prohibited."),
+    S("-!- Please respect the players on the server and their administrator."),
+    S("-!- Enjoy your time on the server and have fun!"),
+    S("-!- If you have any questions, feel free to ask the moderators."),
+    S("-!- Remember to report any issues or bugs you encounter."),
+    S("-!- Be kind and helpful to new players."),
+    S("-!- Cheating and hacking are strictly forbidden and will result in a ban."),
+    S("-!- Respect the privacy and personal information of other players."),
+    S("-!- Use appropriate language and avoid offensive content."),
+    S("-!- Do not share personal or sensitive information on the server."),
+    S("-!- Follow the instructions given by the server staff."),
+    S("-!- Participate in server events and activities to enhance your experience."),
+    S("-!- Help maintain a positive and friendly atmosphere on the server."),
+    S("-!- Thank you for being a part of our community!")
 }
